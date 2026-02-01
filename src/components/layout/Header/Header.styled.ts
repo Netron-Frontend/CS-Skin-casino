19a67ac0a09f410a73eb7148e16f10dfd15ed3aa
Ivan <ivanmakarevic3@gmail.com>
2026-02-01 05:08:22 +0300

    fix: Rolled back changes 2 commits

--- src/components/layout/Header/Header.styled.ts
+++ src/components/layout/Header/Header.styled.ts
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const HeaderBox = styled.header`
     height: 82px;
+		width: 100%;
 		max-width: 100%;
     filter: drop-shadow(0px 0px 4px #3D3D3D);
-		position: relative;
+		position: sticky;
 		top: 0;
 		left: 0;
-		z-index: 1;
+		z-index: 2;
 `
--- src/components/layout/Header/HeaderContent/HeaderContent.tsx
+++ src/components/layout/Header/HeaderContent/HeaderContent.tsx
@@ -4,13 +4,13 @@ import Container from '@mui/material/Container';
 const HeaderContent = () => {
   return (
     <HeaderContentBox>
-        <Container sx={{display: 'flex', justifyContent: 'space-between'}}>
+        <Container maxWidth="lg" sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
           <a href="/" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center' }}><img src="/HomePage/logo.svg" alt=""/>CS Skin casino</a>
           <div style={{display: 'flex', justifyContent: 'space-between', gap: '14px', alignItems: 'center', marginRight: 'auto', marginLeft: '68px'}}>
-              <a href="#" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center' }}><img style={{width: '24px', height: '24px'}} src="/HomePage/HeaderContent/users.svg" alt="users"/>111111</a>
-              <a href="#" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center' }}><img style={{width: '24px', height: '24px'}} src="/HomePage/HeaderContent/gift.svg" alt="gift"/>Daily Free</a>
-              <a href="#" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center' }}><img style={{width: '24px', height: '24px'}} src="/HomePage/HeaderContent/ranks.svg" alt="ranks"/>Ranks</a>
-              <a href="#" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center' }}><img style={{width: '24px', height: '24px'}} src="/HomePage/HeaderContent/help.svg" alt="help"/>Help</a>
+              <a href="#" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center', fontSize: '12px', fontFamily: "Libre Franklin, sans-serif", fontOpticalSizing: 'auto', fontWeight: '400'}}><img style={{width: '24px', height: '24px'}} src="/HomePage/HeaderContent/users.svg" alt="users"/>111111</a>
+              <a href="#" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center', fontSize: '12px', fontFamily: "Libre Franklin, sans-serif", fontOpticalSizing: 'auto', fontWeight: '400'}}><img style={{width: '24px', height: '24px'}} src="/HomePage/HeaderContent/gift.svg" alt="gift"/>Daily Free</a>
+              <a href="#" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center', fontSize: '12px', fontFamily: "Libre Franklin, sans-serif", fontOpticalSizing: 'auto', fontWeight: '400'}}><img style={{width: '24px', height: '24px'}} src="/HomePage/HeaderContent/ranks.svg" alt="ranks"/>Ranks</a>
+              <a href="#" style={{display: 'flex', justifyContent: 'space-between', gap: '3px', alignItems: 'center', fontSize: '12px', fontFamily: "Libre Franklin, sans-serif", fontOpticalSizing: 'auto', fontWeight: '400'}}><img style={{width: '24px', height: '24px'}} src="/HomePage/HeaderContent/help.svg" alt="help"/>Help</a>
           </div>
           <div style={{display: 'flex', justifyContent: 'space-between', gap: '15px', alignItems: 'center'}}>
               <div>
